a582b79cf6e7039b5db39ff24bb44d25b7cd8694
Khrolenko Vladyslav <VLKhrolenko@gmail.com>
2023-05-31 21:04:51 +0300

    Hm1

---
+++ JS/script.js
@@ -0,0 +1,3 @@
+let number1= prompt('Введите первое число:');
+let number2= prompt('Введите второе число:');
+alert (number1-number2)
---
+++ JS/script2.js
@@ -0,0 +1,4 @@
+let firstName = prompt('Введите имя:');
+let midleName = prompt('Введите отчество');
+let lastName = prompt('Введите фамилию:');
+alert (firstName + ' ' + midleName + ' ' +lastName)
